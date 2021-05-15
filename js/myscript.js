d5fefbd776c8fac30b6660dc3638dbe3250f4728
gabahina <patrusayagabahina@gmail.com>
2021-05-15 22:55:17 +0530

    all india release

--- js/myscript.js
+++ js/myscript.js
@@ -1,22 +1,90 @@
 
 var myVar ;
+var myState ;
+var myStateName ;
 var myDistrict ;
 var myDistName ;
 var myAge ;
 
+var states = {"states":[{"state_id":1,"state_name":"Andaman and Nicobar Islands"},{"state_id":2,"state_name":"Andhra Pradesh"},{"state_id":3,"state_name":"Arunachal Pradesh"},{"state_id":4,"state_name":"Assam"},{"state_id":5,"state_name":"Bihar"},{"state_id":6,"state_name":"Chandigarh"},{"state_id":7,"state_name":"Chhattisgarh"},{"state_id":8,"state_name":"Dadra and Nagar Haveli"},{"state_id":37,"state_name":"Daman and Diu"},{"state_id":9,"state_name":"Delhi"},{"state_id":10,"state_name":"Goa"},{"state_id":11,"state_name":"Gujarat"},{"state_id":12,"state_name":"Haryana"},{"state_id":13,"state_name":"Himachal Pradesh"},{"state_id":14,"state_name":"Jammu and Kashmir"},{"state_id":15,"state_name":"Jharkhand"},{"state_id":16,"state_name":"Karnataka"},{"state_id":17,"state_name":"Kerala"},{"state_id":18,"state_name":"Ladakh"},{"state_id":19,"state_name":"Lakshadweep"},{"state_id":20,"state_name":"Madhya Pradesh"},{"state_id":21,"state_name":"Maharashtra"},{"state_id":22,"state_name":"Manipur"},{"state_id":23,"state_name":"Meghalaya"},{"state_id":24,"state_name":"Mizoram"},{"state_id":25,"state_name":"Nagaland"},{"state_id":26,"state_name":"Odisha"},{"state_id":27,"state_name":"Puducherry"},{"state_id":28,"state_name":"Punjab"},{"state_id":29,"state_name":"Rajasthan"},{"state_id":30,"state_name":"Sikkim"},{"state_id":31,"state_name":"Tamil Nadu"},{"state_id":32,"state_name":"Telangana"},{"state_id":33,"state_name":"Tripura"},{"state_id":34,"state_name":"Uttar Pradesh"},{"state_id":35,"state_name":"Uttarakhand"},{"state_id":36,"state_name":"West Bengal"}],"ttl":24};
+
+// $("#dropdownStates").click(function(){     });
+
 $( document ).ready(function() {
-    $(".dropdown-menu li a").click(function(){
+    for( var index in states.states ) {
+        //console.log("in state drop down");
+        var id = states.states[index].state_id ;
+        var name = states.states[index].state_name ;
+       // $('#dropdownStates ul ').append('<li><a href="#" onclick="setState('+id+')" data-value="'+id+'">'+name+'</a></li>');
+          $('#dropdownStates ul ').append('<li><a href="#" onclick="setState('+id+ ',\''+ name  + '\')" data-value="'+id+'">'+name+'</a></li>');
+      //  $('#dropdownDist ul ').append('<li><a href="#" onclick="setDistrict('+id + ',\''+ name  + '\' )"  data-value="' + id  + '">' + name+'</a></li>');
+              
+            }
+
+    // $("#dropdownStates ul li a").click(function(){
+    //     console.log( "which state is clicked: " + this);
+    //     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    //     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    //   });
+
+    // $("#dropdownDist ul li a").click(function(){
+    //     console.log( "which district is clicked: " + this);
+    //     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    //     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    //   });
+
+      $("#dropdownAge ul li a").click(function(){
+        console.log( "which age is clicked: " + this);
         $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
         $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
       });
 
+     //on dynamic data
+      $("#dropdownStates ul").on("click", "li", function(){
+        console.log( "which state is clicked: " + this);
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+      });
+
+      //on dynamic data
+      $("#dropdownDist ul").on("click", "li", function(){
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+
 });
 
-function updateScroll(){
-    var element = document.getElementById("mycomments");
-    //console.log(element.innerHTML);
-    element.scrollTop = element.scrollHeight;
+function getDistrict () { 
+    console.log( "getdistrict is called: " + myState  );
+    var myurl = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+ myState ;
+    console.log(myurl);
+   $.ajax({
+       type: "get",
+       url: myurl,
+       dataType: "json", 
+       success: function(data){
+        console.log("in dist drop down populating " + data);
+        $('#dropdownDist ul ').empty();
+        for( var index in data.districts ) {
+              var id = data.districts[index].district_id ;
+              var name = data.districts[index].district_name ;
+             // $('#dropdownDist ul ').append('<li><a href="#" onclick="setDistrict('+id + ')"  data-value="' + id  + '">' + name+'</a></li>');
+              $('#dropdownDist ul ').append('<li><a href="#" onclick="setDistrict('+id + ',\''+ name  + '\' )"  data-value="' + id  + '">' + name+'</a></li>');
+                                      //('<li><a href="#" onclick="setState('+states.states[index].state_id+')" data-value="'+states.states[index].state_id+'">'+states.states[index].state_name+'</a></li>');
+                                          // <li><a href="#" onclick="setDistrict(140, 'Delhi')" data-value="Delhi">Delhi</a></li> 
+           }
+       }
+   }) ;
 }
+
+
+
+function setState (stateId, statename) {
+    console.log( "State is set" + this);
+    myState = stateId ;
+    myStateName = statename;
+    getDistrict ();
+    }
 
 function setDistrict (id, city) {
      myDistrict = id ;
@@ -27,16 +95,23 @@ function setAge (age) {
     myAge = age ;
     }
 
-function startData () {
-        if ( myDistrict == undefined ){
-            $(".alert").show();
-            $(".alert").empty();
-            $(".alert").append('<span>Please select the District.</span>');
+
+
+function startMonitoring () {
+        if ( myState == undefined ){
+            $("#myalerts").show();
+            $("#myalerts").empty();
+            $("#myalerts").append('<span>Please select the State.</span>');
+             
+        }else if ( myDistrict == undefined ){
+            $("#myalerts").show();
+            $("#myalerts").empty();
+            $("#myalerts").append('<span>Please select the District.</span>');
              
         }else if ( myAge == undefined ){
-            $(".alert").show();
-            $(".alert").empty();
-            $(".alert").append('<span>Please select the Age.</span>');
+            $("#myalerts").show();
+            $("#myalerts").empty();
+            $("#myalerts").append('<span>Please select the Age.</span>');
              
         } else  if( myDistrict != undefined  && myAge != undefined ){
             getData();
@@ -44,17 +119,39 @@ function startData () {
               clearInterval(myVar);
             }
             myVar = setInterval(getData, 3000);
-            $(".alert").hide();
-            $("p").append("<br> <b>Monitoring is started</b>.<br>");
+            $("#myalerts").hide();
+           // $("p").css("background-color", "rgb(0,0,255)");
+            $("p").append("<br> <b>"+ myStateName + " : " +myDistName + ", above: " + myAge +" years. Monitoring is started</b>.<br>");
             updateScroll();
             }
     }
 
-function stopData () {
-clearInterval(myVar);
-$("p").append("<br> <b>Monitoring is stopped</b>.<br>");
-updateScroll();
-}
+function stopMonitoring () {
+    clearInterval(myVar);
+    $("p").append("<br> <b>Monitoring is stopped</b>.<br>");
+    updateScroll();
+    }
+
+function clearlog () {
+    
+    $("p").empty();
+    $("p").append("<br> <b>Log is cleared</b>.<br>");
+    updateScroll();
+    }
+
+function updateScroll(){
+    var gettext = $("p").text() ;
+    console.log(gettext);
+    if (gettext.length > 5000){
+        getsubtext = gettext.substring(3000, 5000);
+       $("p").empty();
+       $("p").append(getsubtext);
+    }
+
+    var element = document.getElementById("mylog");
+    //console.log(element.innerHTML);
+    element.scrollTop = element.scrollHeight;
+   }
 
 function getData () { 
     var today = new Date();
@@ -89,11 +186,11 @@ function getData () {
                 var center = data["centers"][i];
                 for(var x = 0; x < center["sessions"].length; x++) {
                     var y = center["sessions"][x];
-                    if (y["min_age_limit"] <= myAge && y["available_capacity"] > 0 ){
+                    if (y["min_age_limit"] == myAge && y["available_capacity"] > 0 ){
                     // console.log("Vaccine available: " + y["vaccine"] + " at " +  center["address"] + "for age above " + 
                     //   + y["min_age_limit"] + " on date: " + y["date"] );
-                    
-                      $("p").append( "Vaccine available: " + y["vaccine"] + " at " +  center["address"] + "for age above " + 
+                    var todayDate = new Date().toUTCString();
+                      $("p").append(todayDate+ " " + y["vaccine"] + " at " +  center["address"] + " for age above " + 
                       + y["min_age_limit"] + " on date: " + y["date"] + ".<br>");
                       count = count + 1 ;
                       updateScroll();
@@ -114,6 +211,7 @@ function getData () {
      
  
 }
+
 
 
 
